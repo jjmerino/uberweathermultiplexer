a66ca904aced8ba117b1633f2c49cfdbe3f410d6
Jose Merino <jomerinog@gmail.com>
2014-10-02 20:40:00 -0700

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('uberingApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
